2f20c412383c6a4e97e21c9f14ea07a72d4551a6
Jimmy Leta <jimmy.leta@gmail.com>
2022-02-14 06:46:17 -0500

    Moving declaration of loop counters to inside the scope of individual for loop

--- src/fem1d.cpp
+++ src/fem1d.cpp
@@ -14,11 +14,13 @@ int main() {
   double arite[NSUB + 1];
   double f[NSUB + 1];
   double h[NSUB];
+
   int ibc;
   int indx[NSUB + 1];
   int node[NL * NSUB];
   int nquad;
   int nu;
+
   double ul;
   double ur;
   double xl;
@@ -42,62 +44,57 @@ int main() {
   std::cout << "  The interval [XL,XR] is broken into NSUB = " << NSUB
             << " subintervals\n";
   std::cout << "  Number of basis functions per element is NL = " << NL << "\n";
+
   //
   //  Initialize the data that defines the problem.
   //
   init(&ibc, &nquad, &ul, &ur, &xl, &xr);
+
   //
   //  Compute the quantities which define the geometry of the
   //  problem.
   //
   geometry(h, ibc, indx, NL, node, NSUB, &nu, xl, xn, xquad, xr);
+
   //
   //  Assemble the linear system.
   //
   assemble(adiag, aleft, arite, f, h, indx, NL, node, nu, nquad, NSUB, ul, ur,
            xn, xquad);
+
   //
   //  Print out the linear system.
   //
   prsys(adiag, aleft, arite, f, nu);
+
   //
   //  Solve the linear system.
   //
   solve(adiag, aleft, arite, f, nu);
+
   //
   //  Print out the solution.
   //
   output(f, ibc, indx, NSUB, nu, ul, ur, xn);
+
   //
   //  Terminate.
   //
   std::cout << "\n";
   std::cout << "FEM1D:\n";
   std::cout << "  Normal end of execution.\n";
-
   std::cout << "\n";
+
   timestamp();
 
   return 0;
-#undef NL
-#undef NSUB
 }
-//****************************************************************************80
 
 void assemble(double adiag[], double aleft[], double arite[], double f[],
               double h[], int indx[], int nl, int node[], int nu, int nquad,
               int nsub, double ul, double ur, double xn[], double xquad[]) {
   double aij;
   double he;
-  int i;
-  int ie;
-  int ig;
-  int il;
-  int iq;
-  int iu;
-  int jg;
-  int jl;
-  int ju;
   double phii;
   double phiix;
   double phij;
@@ -110,37 +107,37 @@ void assemble(double adiag[], double aleft[], double arite[], double f[],
   //  Zero out the arrays that hold the coefficients of the matrix
   //  and the right hand side.
   //
-  for (i = 0; i < nu; i++) {
+  for (int i = 0; i < nu; i++) {
     f[i] = 0.0;
   }
-  for (i = 0; i < nu; i++) {
+  for (int i = 0; i < nu; i++) {
     adiag[i] = 0.0;
   }
-  for (i = 0; i < nu; i++) {
+  for (int i = 0; i < nu; i++) {
     aleft[i] = 0.0;
   }
-  for (i = 0; i < nu; i++) {
+  for (int i = 0; i < nu; i++) {
     arite[i] = 0.0;
   }
   //
   //  For interval number IE,
   //
-  for (ie = 0; ie < nsub; ie++) {
+  for (int ie = 0; ie < nsub; ie++) {
     he = h[ie];
     xleft = xn[node[0 + ie * 2]];
     xrite = xn[node[1 + ie * 2]];
     //
     //  consider each quadrature point IQ,
     //
-    for (iq = 0; iq < nquad; iq++) {
+    for (int iq = 0; iq < nquad; iq++) {
       xquade = xquad[ie];
       //
       //  and evaluate the integrals associated with the basis functions
       //  for the left, and for the right nodes.
       //
-      for (il = 1; il <= nl; il++) {
-        ig = node[il - 1 + ie * 2];
-        iu = indx[ig] - 1;
+      for (int il = 1; il <= nl; il++) {
+        int ig = node[il - 1 + ie * 2];
+        int iu = indx[ig] - 1;
 
         if (0 <= iu) {
           phi(il, xquade, &phii, &phiix, xleft, xrite);
@@ -160,9 +157,9 @@ void assemble(double adiag[], double aleft[], double arite[], double f[],
           //  function times itself, or times the other basis function
           //  that is nonzero in this interval.
           //
-          for (jl = 1; jl <= nl; jl++) {
-            jg = node[jl - 1 + ie * 2];
-            ju = indx[jg] - 1;
+          for (int jl = 1; jl <= nl; jl++) {
+            int jg = node[jl - 1 + ie * 2];
+            int ju = indx[jg] - 1;
 
             phi(jl, xquade, &phij, &phijx, xleft, xrite);
 
@@ -211,14 +208,13 @@ double ff(double x) {
 
 void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
               int *nu, double xl, double xn[], double xquad[], double xr) {
-  int i;
   //
   //  Set the value of XN, the locations of the nodes.
   //
   std::cout << "\n";
   std::cout << "  Node      Location\n";
   std::cout << "\n";
-  for (i = 0; i <= nsub; i++) {
+  for (int i = 0; i <= nsub; i++) {
     xn[i] = ((double)(nsub - i) * xl + (double)i * xr) / (double)(nsub);
     std::cout << "  " << std::setw(8) << i << "  " << std::setw(14) << xn[i]
               << "\n";
@@ -229,7 +225,7 @@ void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
   std::cout << "\n";
   std::cout << "Subint    Length\n";
   std::cout << "\n";
-  for (i = 0; i < nsub; i++) {
+  for (int i = 0; i < nsub; i++) {
     h[i] = xn[i + 1] - xn[i];
     std::cout << "  " << std::setw(8) << i + 1 << "  " << std::setw(14) << h[i]
               << "\n";
@@ -241,7 +237,7 @@ void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
   std::cout << "\n";
   std::cout << "Subint    Quadrature point\n";
   std::cout << "\n";
-  for (i = 0; i < nsub; i++) {
+  for (int i = 0; i < nsub; i++) {
     xquad[i] = 0.5 * (xn[i] + xn[i + 1]);
     std::cout << "  " << std::setw(8) << i + 1 << "  " << std::setw(14)
               << xquad[i] << "\n";
@@ -253,7 +249,7 @@ void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
   std::cout << "\n";
   std::cout << "Subint  Left Node  Right Node\n";
   std::cout << "\n";
-  for (i = 0; i < nsub; i++) {
+  for (int i = 0; i < nsub; i++) {
     node[0 + i * 2] = i;
     node[1 + i * 2] = i + 1;
     std::cout << "  " << std::setw(8) << i + 1 << "  " << std::setw(8)
@@ -268,7 +264,7 @@ void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
   //
   //  Handle first node.
   //
-  i = 0;
+  int i = 0;
   if (ibc == 1 || ibc == 3) {
     indx[i] = -1;
   } else {
@@ -299,8 +295,8 @@ void geometry(double h[], int ibc, int indx[], int nl, int node[], int nsub,
   std::cout << "\n";
   std::cout << "  Node  Unknown\n";
   std::cout << "\n";
-  
-  for (i = 0; i <= nsub; i++) {
+
+  for (int i = 0; i <= nsub; i++) {
     std::cout << "  " << std::setw(8) << i << "  " << std::setw(8) << indx[i]
               << "\n";
   }
@@ -371,7 +367,7 @@ void output(double f[], int ibc, int indx[], int nsub, int nu, double ul,
   std::cout << "  Node    X(I)        U(X(I))\n";
   std::cout << "\n";
 
-  for (i = 0; i <= nsub; i++) {
+  for (int i = 0; i <= nsub; i++) {
     //
     //  If we're at the first node, check the boundary condition.
     //
@@ -445,7 +441,7 @@ void prsys(double adiag[], double aleft[], double arite[], double f[], int nu) {
   std::cout << "Equation  ALEFT  ADIAG  ARITE  RHS\n";
   std::cout << "\n";
 
-  for (i = 0; i < nu; i++) {
+  for (int i = 0; i < nu; i++) {
     std::cout << "  " << std::setw(8) << i + 1 << "  " << std::setw(14)
               << aleft[i] << "  " << std::setw(14) << adiag[i] << "  "
               << std::setw(14) << arite[i] << "  " << std::setw(14) << f[i]
@@ -464,14 +460,13 @@ double qq(double x) {
 }
 
 void solve(double adiag[], double aleft[], double arite[], double f[], int nu) {
-  int i;
   //
   //  Carry out Gauss elimination on the matrix, saving information
   //  needed for the backsolve.
   //
   arite[0] = arite[0] / adiag[0];
 
-  for (i = 1; i < nu - 1; i++) {
+  for (int i = 1; i < nu - 1; i++) {
     adiag[i] = adiag[i] - aleft[i] * arite[i - 1];
     arite[i] = arite[i] / adiag[i];
   }
@@ -481,16 +476,16 @@ void solve(double adiag[], double aleft[], double arite[], double f[], int nu) {
   //  matrix.
   //
   f[0] = f[0] / adiag[0];
-  for (i = 1; i < nu; i++) {
+  for (int i = 1; i < nu; i++) {
     f[i] = (f[i] - aleft[i] * f[i - 1]) / adiag[i];
   }
   //
   //  And now carry out the steps of "back substitution".
   //
-  for (i = nu - 2; 0 <= i; i--) {
+  for (int i = nu - 2; 0 <= i; i--) {
     f[i] = f[i] - arite[i] * f[i + 1];
   }
-  return;
+
 }
 
 void timestamp() {
